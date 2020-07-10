420339facecce3a17b9403e184f0a48894c43ef9
Charlie Hsieh <charlieofficial@gmail.com>
2020-07-10 13:48:01 +0800

    try script

---
+++ utils/install.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#hash git >/dev/null &&  git clone https://github.com/charlietag/vim_settings.git ~/.vim || {
+#  echo "Sorry, Git is not installed yet!"
+#  exit
+#}
+
+#rm -f $HOME/.vimrc
+#rm -fr $HOME/.vim
+#
+#curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+#    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+echo "$HOME"
